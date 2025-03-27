2c6c9100723bbeec5a80eb4d7da46ace8329276a
Emre Şahiner <emre@workintech.com.tr>
2025-03-27 12:33:53 +0300

    Gemini added

--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -2,6 +2,9 @@ import React, { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 import { useForm } from "react-hook-form";
 import { usePostTweet } from "../services/mutations";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWandMagicSparkles } from "@fortawesome/free-solid-svg-icons";
 
 function NewPostForm() {
   const { user } = useContext(UserContext);
@@ -9,6 +12,8 @@ function NewPostForm() {
     register,
     handleSubmit,
     reset,
+    getValues,
+    setValue,
     formState: { errors },
   } = useForm({ defaultValues: { post: "" }, mode: "onChange" });
 
@@ -19,21 +24,58 @@ function NewPostForm() {
     reset();
   }
 
+  const KEY = import.meta.env.VITE_GEMINI_API;
+  const genAI = new GoogleGenerativeAI(KEY);
+
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+    systemInstruction: "convert to english",
+  });
+
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseModalities: [],
+    responseMimeType: "text/plain",
+  };
+
+  async function run() {
+    const prompt = getValues("post");
+    setValue("post", "Translating...");
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [],
+    });
+
+    const result = await chatSession.sendMessage(prompt);
+    setValue("post", result.response.text());
+  }
+
   return (
     <form
       onSubmit={handleSubmit(submitFn)}
       className="flex gap-4 justify-between items-start mb-4"
     >
       <img src={user.avatar} className="rounded-full w-12" />
-      <textarea
-        {...register("post", {
-          minLength: { value: 3, message: "Lütfen mesajınızı yazınız." },
-        })}
-        className="w-full border border-slate-200"
-      >
-        {" "}
-      </textarea>
+      <div className="relative flex-grow">
+        <textarea
+          {...register("post", {
+            minLength: { value: 3, message: "Lütfen mesajınızı yazınız." },
+          })}
+          className=" w-full border border-slate-200"
+          rows={5}
+        >
+          {" "}
+        </textarea>
+        <div className="absolute right-2 bottom-3" onClick={run}>
+          <FontAwesomeIcon icon={faWandMagicSparkles} />
+        </div>
+      </div>
+
       <button>Post</button>
+
       {errors.tweet && <p>{errors.tweet.message}</p>}
       {mutation.isPending && "Posting data..."}
       {mutation.error && mutation.error.message}
